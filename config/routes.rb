54e87f69c6324b41cefe0c6ef3272f1d44d10a14
tienbian <tientranvu@gmail.com>
2015-07-02 10:06:12 +0700

    create post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :games
   resources :competitions
   resources :teams
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630025430) do
+ActiveRecord::Schema.define(version: 20150702030335) do
 
   create_table "competitions", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 20150630025430) do
     t.text     "description", limit: 65535
     t.string   "picture",     limit: 255
     t.integer  "team_id",     limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.string   "description", limit: 255
+    t.string   "text",        limit: 255
+    t.text     "body",        limit: 65535
+    t.integer  "user_id",     limit: 4
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
